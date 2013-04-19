2b57d24445606bb41014358aca0170e1c7e8310e
Rob Harrop <rob.harrop@gmail.com>
2013-04-19 17:10:39 +0100

    First commit of the basic Spring REST app

---
+++ src/main/java/app/DummyBookRepository.java
@@ -0,0 +1,53 @@
+package app;
+
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * A very noddy implementation of the {@see BookRepository} interface.
+ */
+@Component
+class DummyBookRepository implements BookRepository {
+  private final Map<Integer, Book> books = new ConcurrentHashMap<Integer, Book>();
+
+  @Override
+  public Book findById(Integer id) {
+    return this.books.get(id);
+  }
+
+  @Override
+  public List<Book> findAll() {
+    List<Book> books = new ArrayList<Book>(this.books.values());
+    Collections.sort(books, new Comparator<Book>() {
+      @Override
+      public int compare(Book o1, Book o2) {
+        return o1.getId() - o2.getId();
+      }
+    });
+    return books;
+  }
+
+  @Override
+  public Book save(Book book) {
+    if (book.getId() == null) {
+      book.setId(nextId());
+    }
+    this.books.put(book.getId(), book);
+    return book;
+  }
+
+  @Override
+  public void delete(Integer id) {
+    this.books.remove(id);
+  }
+
+  private Integer nextId() {
+    if (this.books.isEmpty()) {
+      return 1;
+    } else {
+      return Collections.max(this.books.keySet()) + 1;
+    }
+  }
+}
---
+++ src/test/java/app/BookControllerRequestTests.java
@@ -0,0 +1,97 @@
+package app;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/main/webapp/WEB-INF/app-servlet.xml")
+public class BookControllerRequestTests {
+  private MockMvc mockMvc;
+
+  @SuppressWarnings("SpringJavaAutowiringInspection")
+  @Autowired
+  protected WebApplicationContext wac;
+
+  @Autowired
+  protected BookRepository repository;
+
+  @Autowired
+  protected ConversionService conversionService;
+
+  @Before
+  public void setup() {
+    this.mockMvc = webAppContextSetup(this.wac).build();
+    this.repository.save(new Book("Pro Spring", new Author("Rob", "Harrop")));
+  }
+
+  @Test
+  public void listBooks() throws Exception {
+    mockMvc.perform(get("/books"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].title").value("Pro Spring"))
+        .andExpect(jsonPath("$[0].author.firstName").value("Rob"));
+  }
+
+  @Test
+  public void getBook() throws Exception {
+    mockMvc.perform(get("/books/{id}", 1))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.title").value("Pro Spring"));
+  }
+
+  @Test
+  public void getUnknownBook() throws Exception {
+    mockMvc.perform(get("/books/{id}", 100))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void deleteBook() throws Exception {
+    mockMvc.perform(delete("/books/{id}", 1))
+        .andExpect(status().isNoContent());
+    assertNull(this.repository.findById(1));
+  }
+
+  @Test
+  public void createBook() throws Exception {
+    Book book = new Book("Pro Struts", new Author("Rob", "Harrop"));
+    ObjectMapper mapper = new ObjectMapper();
+    String content = mapper.writeValueAsString(book);
+    mockMvc.perform(post("/books")
+        .content(content)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isCreated());
+    assertNotNull(this.repository.findById(2));
+  }
+
+  @Test
+  public void updateBook() throws Exception {
+    Book book = new Book("Pro Struts", new Author("Rob", "Harrop"));
+    ObjectMapper mapper = new ObjectMapper();
+    String content = mapper.writeValueAsString(book);
+    mockMvc.perform(put("/books/{id}", 1)
+        .content(content)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNoContent());
+    assertEquals("Pro Struts", this.repository.findById(1).getTitle());
+  }
+}
